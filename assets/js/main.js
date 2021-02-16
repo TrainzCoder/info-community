c7e7f8f1bd3756a350129d826225c2fe785589c5
TrainzCoder <prawira206jng@gmail.com>
2021-02-16 20:17:29 +0700

    Mark The LDBI

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// Type Animation
+
+let typed = new Typed('.typed', {
+  strings: ['Ekstrakurikuler ', 'Daring SMAN CMBBS'],
+  loop:Infinity,
+  backSpeed: 70,
+  startDelay: 1100,
+  showCursor:false,
+  typeSpeed: 80
+});
